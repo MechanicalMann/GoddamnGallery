20aa4d79c31e77f5509caefe99a767fa5d9f5ac5
malorisdead <malorisdead@gmail.com>
2014-05-06 07:31:09 -0400

    Scraping now works (barely)

---
+++ gallery.sql
@@ -0,0 +1,53 @@
+
+-- Table: images
+CREATE TABLE images ( 
+    id    INTEGER         PRIMARY KEY AUTOINCREMENT,
+    path  VARCHAR( 255 )  NOT NULL,
+    thumb VARCHAR( 255 ),
+    x     INTEGER,
+    y     INTEGER,
+    color VARCHAR( 6 ) 
+);
+
+-- Table: tags
+CREATE TABLE tags ( 
+    id   INTEGER         PRIMARY KEY,
+    name VARCHAR( 128 )  NOT NULL
+                         UNIQUE,
+    slug VARCHAR( 128 )  NOT NULL
+                         UNIQUE 
+);
+
+-- Table: tag_image
+CREATE TABLE tag_image ( 
+    id       INTEGER PRIMARY KEY,
+    image_id INTEGER NOT NULL
+                     REFERENCES images ( id ) ON DELETE CASCADE,
+    tag_id   INTEGER NOT NULL
+                     REFERENCES tags ( id ) ON DELETE CASCADE 
+);
+
+-- View: images_by_tag
+CREATE VIEW images_by_tag AS
+       SELECT t.name AS tag_name,
+              t.slug AS tag_slug,
+              i.*
+         FROM tags t
+              LEFT JOIN tag_image ti
+                     ON ti.tag_id = t.id
+              LEFT JOIN images i
+                     ON i.id = ti.image_id;
+;
+
+
+-- View: tags_by_image
+CREATE VIEW tags_by_image AS
+       SELECT i.path,
+              t.*
+         FROM images i
+              LEFT JOIN tag_image ti
+                     ON ti.image_id = i.id
+              LEFT JOIN tags t
+                     ON t.id = ti.tag_id;
+;
+
--- gdg/scrape.py
+++ gdg/scrape.py
@@ -5,45 +5,59 @@ from peewee import *
 import gdg
 from gdg.data import *
 
-config = ConfigParser()
+config = ConfigParser.ConfigParser()
 
 def get_files(path):
+    images = []
     for root, dirs, files in os.walk(path):
         for filename in files:
-            yield os.path.join(root, filename)
+            images.append(os.path.join(root, filename))
+    return images
 
 def get_thumb(path, base, ext):
-    return os.path.join(path, base + ext);
+    return os.path.join(path, base + ext)
 
-def main():
+def get_directory(path):
+    path = path.translate(None, '"\'')
+    if (path == None or path == 'gdg.current_dir'):
+        return gdg.current_dir
+    if (os.path.isdir(path)):
+        return os.path.abspath(path)
+    return os.path.join(gdg.current_dir, path)
+
+def scrape_images():
     config.read(os.path.join(gdg.current_dir, 'gdg.conf'))
-    dbpath = config['database']['path']
+    dbpath = get_directory(config.get('database', 'path'))
+
+    print(dbpath)
     
     with GoddamnDatabase(dbpath) as db:
-        current = Image.select(Image.path)
-        ondisk = get_files(config['images']['path'])
-        new = set(ondisk) - set(current)
-        deleted = set(current) - set(ondisk)
+        current = [i.path for i in Image.select()]
+        ondisk = get_files(get_directory(config.get('images', 'path')))
+        new_files = set(ondisk) - set(current)
+        deleted_files = set(current) - set(ondisk)
         
-        with db.transaction():
-            for f in new:
-                i = Image()
-                i.path = f
-                i.save()
-            
-        with db.transaction():
-            for f in deleted:
-                Image.delete().where(Image.path == f).execute()
+        if len(new_files) > 0:
+            with db.transaction():
+                for f in new_files:
+                    i = Image()
+                    i.path = f
+                    i.save()
+                    
+        if len(deleted_files) > 0:
+            with db.transaction():
+                for f in deleted_files:
+                    Image.delete().where(Image.path == f).execute()
     
     with GoddamnDatabase(dbpath) as db:
         for i in Image.select().where(Image.thumb == None):
-            img = Image.open(PIL.Image.open(i.path))
+            img = PIL.Image.open(i.path)
             x = 0
             w = min(img.size)
             h = w
             
             if (img.size[0] > img.size[1]):
-                x = int((i.size[0] - w) / 2)
+                x = int((img.size[0] - w) / 2)
             
             box = (x, 0, x + w, h)
             
@@ -52,15 +66,16 @@ def main():
             
             path_parts = os.path.split(i.path)
             name_parts = os.path.splitext(path_parts[1])
+            thumb_path = get_directory(config.get('thumbnails', 'path'))
             
-            thumb_name = config['thumbnails']['prefix'] + name_parts[0] + config['thumbnails']['postfix']
+            thumb_name = config.get('thumbnails', 'prefix').translate(None, '"\'') + name_parts[0] + config.get('thumbnails', 'postfix').translate(None, '"\'')
             
-            thumb = get_thumb(path_parts[0], thumb_name, name_parts[1])
+            thumb = get_thumb(thumb_path, thumb_name, name_parts[1])
             
             counter = 1
             while os.path.isfile(thumb):
                 thumb_name = thumb_name + counter
-                thumb = get_thumb(path_parts[0], thumb_name, name_parts[1])
+                thumb = get_thumb(thumb_path, thumb_name, name_parts[1])
                 
             t.save(thumb)
             
