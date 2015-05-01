2a92d42bc9d2934328cdd338b8cb8ae0d693e360
malorisdead <malorisdead@gmail.com>
2015-05-01 17:17:01 -0400

    Updates API to allow extension-specific searching.

--- gdg/__init__.py
+++ gdg/__init__.py
@@ -115,7 +115,18 @@ def find_image(name):
         
     baseurl = urljoin(cherrypy.request.base, cherrypy.request.script_name + '/')
     dbpath = cherrypy.request.app.config['database']['path']
-    pattern = "[\\\/]" + re.escape(name) + "\..{2,}$"
+    
+    ext = "\..{2,}$"
+    pattern = "[\\\/]" + re.escape(name)
+    
+    # If they include an extension in their search, search for their input exactly.
+    # If they did not include an extension, search for images with any extension.
+    if re.search(ext, name) == None:
+        pattern += ext
+    else:
+        pattern += "$"
+    
+    print("Regex: " + pattern)
     
     with GoddamnDatabase(dbpath):
         return [get_relative_path(baseurl, i.path) for i in Image.select().where(Image.path.regexp(pattern))]
